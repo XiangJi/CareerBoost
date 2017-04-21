950efdc9fd359e5e043ad0ac3d16b844a30b59e4
Xiang Ji <xiang719@gmail.com>
2017-04-21 19:22:37 -0400

    Sync with home

--- src/DSA/Greedy/PatchingArray/Solution.java
+++ src/DSA/Greedy/PatchingArray/Solution.java
@@ -2,9 +2,10 @@ package DSA.Greedy.PatchingArray;
 
 public class Solution {
 
+	// Create a high standard code, test, follow up
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+    	
     }
 
 }
