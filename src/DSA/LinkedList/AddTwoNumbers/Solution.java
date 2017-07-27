06194a1fb8e55ca6d375a9fb03c76319345208ca
xiangji <xiang719@gmail.com>
2017-07-26 20:54:28 -0400

    2

---
+++ src/DSA/LinkedList/AddTwoNumbers/Solution.java
@@ -0,0 +1,46 @@
+package DSA.LinkedList.AddTwoNumbers;
+
+
+// 数学Carry问题的实习要收敛 + 基本链表skills
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+
+        int carry = 0;
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+
+        while (l1 != null && l2 != null) {
+            int sum = carry + l1.val + l2.val;
+            p.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            p = p.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            int sum = carry + l1.val;
+            p.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            p = p.next;
+            l1 = l1.next;
+        }
+
+        while (l2!= null) {
+            int sum = carry + l2.val;
+            p.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            p = p.next;
+            l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            p.next = new ListNode(carry);
+        }
+
+        return head.next;
+    }
+}
---
+++ src/DSA/LinkedList/Summary.md
@@ -0,0 +1,19 @@
+# Linked list
+
+Basic operation
+
+
+
+Two pointers
+
+
+
+fast slow pointers
+
+
+
+空间想象 画图法
+
+
+
+用head node保留一个引用，最后想返回什么就返回什么
