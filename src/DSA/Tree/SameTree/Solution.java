46ce85cbbc7c4666b9845f34ab647e9aaec75632
xiangji <xiang719@gmail.com>
2017-09-08 16:00:57 -0400

    u

--- src/DSA/Tree/SameTree/Solution.java
+++ src/DSA/Tree/SameTree/Solution.java
@@ -1,5 +1,21 @@
 package DSA.Tree.SameTree;
 
-public class Solution {
+import DSA.Tree.TreeNode;
 
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+
+        if (p.val == q.val && left && right) {
+            return true;
+        }
+        return false;
+    }
 }
