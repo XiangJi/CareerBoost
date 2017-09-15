cedbbf51dde789b79a2353ec7d3f032a196dc324
xiangji <xiang719@gmail.com>
2017-09-15 15:48:01 -0400

    u

--- src/DSA/String/ValidWordAbbreviation/Solution.java
+++ src/DSA/String/ValidWordAbbreviation/Solution.java
@@ -1,5 +1,36 @@
 package DSA.String.ValidWordAbbreviation;
 
-public class Solution {
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0, j = 0;
+        char[] wd = word.toCharArray();
+        char[] ab =abbr.toCharArray();
+        while (i < word.length() && j < abbr.length()) {
+            if (wd[i] == ab[j]) {
+                i++;
+                j++;
+                continue;
+            }
+            // test case, 'a', '01'
+            if (ab[j] <= '0' || ab[j] > '9') {
+                return false;
+            }
+            int start = j;
+            while (j < abbr.length() && ab[j] >= '0' && ab[j] <= '9') {
+                j++;
+            }
+            int num = Integer.valueOf(abbr.substring(start, j));
+            i += num;
+        }
+
+        return i == word.length() && j == abbr.length();
+    }
+
+    public static void main(String[] args) {
+        String a = "internationalization";
+        String b = "i12iz4n";
+        Solution test = new Solution();
+        test.validWordAbbreviation(a, b);
+    }
 
 }
