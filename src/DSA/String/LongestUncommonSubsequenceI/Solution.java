f20b714d6abed9728eb0e03f5c8f8a880683e167
xiangji <xiang719@gmail.com>
2017-10-02 17:57:43 -0400

    u

---
+++ src/DSA/String/LongestUncommonSubsequenceI/Solution.java
@@ -0,0 +1,7 @@
+package DSA.String.LongestUncommonSubsequenceI;
+
+public class Solution {
+    public int findLUSlength(String a, String b) {
+        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
+    }
+}
