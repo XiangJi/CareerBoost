e0a52127eac61630994b55eb006cacfa0c50ed72
Xiang Ji <xiang719@gmail.com>
2017-11-02 08:52:59 -0400

    u

---
+++ src/DSA/String/EncodeandDecodeStrings/Codec.java
@@ -0,0 +1,39 @@
+package DSA.String.EncodeandDecodeStrings;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Codec {
+    // used when implementing message queue
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        // Write your code here
+        StringBuilder result = new StringBuilder();
+        for(String str : strs){
+            result.append(String.valueOf(str.length()) + "$");
+            result.append(str);
+        }
+        return result.toString();
+    }
+
+    /**
+     * @param str a string
+     * @return dcodes a single string to a list of strings
+     */
+    public List<String> decode(String str) {
+        // Write your code here
+        List<String> result = new LinkedList<String>();
+        int start = 0;
+        while(start < str.length()){
+            int idx = str.indexOf('$', start);
+            int size = Integer.parseInt(str.substring(start, idx));
+            result.add(str.substring(idx + 1, idx + size + 1));
+            start = idx + size + 1;
+        }
+        return result;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
