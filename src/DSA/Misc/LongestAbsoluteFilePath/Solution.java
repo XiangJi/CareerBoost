998b40a5aa8b63ae0a45e58d7ce67ebd42bff6d3
xiangji <xiang719@gmail.com>
2017-11-13 16:24:03 -0500

    Need understand again

--- src/DSA/Misc/LongestAbsoluteFilePath/Solution.java
+++ src/DSA/Misc/LongestAbsoluteFilePath/Solution.java
@@ -6,19 +6,39 @@ package DSA.Misc.LongestAbsoluteFilePath;
  * We are interested in finding the longest (number of characters) absolute path to a file within our file system. For example, in the second example above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext", and its length is 32 (not including the double quotes).
  * Given a string representing the file system in the above format, return the length of the longest absolute path to file in the abstracted file system. If there is no file in the system, return 0.
  * Note:
- * The name of a file contains at least a . and an extension. 
+ * The name of a file contains at least a . and an extension.
  * The name of a directory or sub-directory will not contain a ..
  * Time complexity required: O(n) where n is the size of the input string.
  * Notice that a/aa/aaa/file1.txt is not the longest file path, if there is another path aaaaaaaaaaaaaaaaaaaaa/sth.png.
  */
 public class Solution {
+    /*
+     * @param input: an abstract file system
+     * @return: return the length of the longest absolute path to file
+     */
     public int lengthLongestPath(String input) {
-        
+        // Write your code here
+        if (input.length() == 0) {
+            return 0;
+        }
+        int ans = 0;
+        int[] level_size = new int[input.length() + 1];
+
+        for (String line : input.split("\n")) {
+            int level = line.lastIndexOf('\t') + 2;
+            int len = line.length() - (level - 1);
+            if (line.contains(".")) {
+                ans = Math.max(ans, level_size[level - 1] + len);
+            } else {
+                level_size[level] = level_size[level - 1] + len + 1;
+            }
+        }
+        return ans;
     }
-    
+
     public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Solution test = new Solution();
+        test.lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext");
 
-	}
+    }
 }
-
