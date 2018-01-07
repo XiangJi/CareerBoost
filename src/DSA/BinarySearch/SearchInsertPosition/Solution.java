1a21fc0805ada939d1c16c7ec10a1c2d2b944e46
Xiang Ji <xiang719@gmail.com>
2018-01-06 20:49:09 -0500

    u

---
+++ src/DSA/BinarySearch/SearchInsertPosition/Solution.java
@@ -0,0 +1,36 @@
+package DSA.BinarySearch.SearchInsertPosition;
+/*
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0 || nums == null) {
+            return 0;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] > target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                return mid;
+            }
+        }
+        
+        if (nums[start] >= target) {
+            return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
+        }
+        
+    }
+}
--- src/DSA/LinkedList/MergeTwoSortedLists/Solution.java
+++ src/DSA/LinkedList/MergeTwoSortedLists/Solution.java
@@ -4,20 +4,19 @@ import DSA.LinkedList.ListNode;
 
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-                ListNode head = new ListNode(0);
-                ListNode cur = head;
-                while (l1 != null && l2 != null) {
-                        if (l1.val < l2.val) {
-                                cur.next = l1;
-                                l1 = l1.next;
-                        }
-                        else {
-                                cur.next = l2;
-                                l2 = l2.next;
-                        }
-                        cur = cur.next;
-                }
-                cur.next = (l1 != null) ? l1 : l2;
-                return head.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
         }
+        current.next = (l1 == null) ? l2 : l1;
+        return dummy.next;
+    }
 }
