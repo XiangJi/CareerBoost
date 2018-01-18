f199611945c7b75eedb5208a3adae6094404fa1c
xiangji <xiang719@gmail.com>
2018-01-18 17:56:58 -0500

    u

--- src/DSA/Array/FindAllDuplicatesinanArray/Solution.java
+++ src/DSA/Array/FindAllDuplicatesinanArray/Solution.java
@@ -3,6 +3,27 @@ package DSA.Array.FindAllDuplicatesinanArray;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+
+将其值对应的位置上的那个元素取负数，当然因为元素的值是从1开始的，
+所以变成位置的时候都要减一，每次换成位置时都要用绝对值来算，因为出现了两次的元素，
+在之前就已经被变成负数了，所以借此可以判断，如果该位置的元素是个负数，说明之前出现过一次，就记录下来。
+
+bucket思想
+正负号标记法（一趟遍历）
+ */
 public class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> result = new ArrayList<Integer>();
