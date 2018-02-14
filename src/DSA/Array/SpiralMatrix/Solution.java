ba577d03067ed71363bddce99827d4d3bf74e988
Xiang Ji <xiang719@gmail.com>
2018-02-13 19:19:38 -0500

    u

---
+++ src/DSA/Array/SpiralMatrix/Solution.java
@@ -0,0 +1,61 @@
+package DSA.Array.SpiralMatrix;
+
+import java.util.ArrayList;
+
+/*
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+For example,
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
+
+ * 
+ * O(n * m)
+ */
+// just implementation
+public class Solution {
+    public ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0)
+            return result;
+
+        // use pointers to store position
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1;
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i++) {
+                result.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result.add(matrix[i][colStart]);
+                }
+                colStart++;
+            }
+
+        }
+        return result;
+    }
+}
