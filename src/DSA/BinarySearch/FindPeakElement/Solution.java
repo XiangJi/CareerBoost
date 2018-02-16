02e7f48df549e23544a16e27ced77a9a159f8f11
Xiang Ji <xiang719@gmail.com>
2018-02-15 22:02:00 -0500

    2

---
+++ src/DSA/BinarySearch/FindPeakElement/Solution.java
@@ -0,0 +1,40 @@
+package DSA.BinarySearch.FindPeakElement;
+/*
+ * 
+A peak element is an element that is greater than its neighbors.
+
+Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+
+G tag
+
+搜索的一个局部峰值, 比较和后一位的大小就可以了
+
+ */
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (nums[start] > nums[end]) {
+            return start;
+        }
+        return end;
+    }
+}
