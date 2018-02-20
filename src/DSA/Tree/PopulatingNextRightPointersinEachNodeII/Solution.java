9de01d92f23d27bd300259ea3bb949284469f1d7
Xiang Ji <xiang719@gmail.com>
2018-02-19 19:16:24 -0500

    1

---
+++ src/DSA/Tree/PopulatingNextRightPointersinEachNodeII/Solution.java
@@ -0,0 +1,57 @@
+package DSA.Tree.PopulatingNextRightPointersinEachNodeII;
+
+import DSA.Tree.TreeLinkNode;
+
+/*
+ * Follow up for problem "Populating Next Right Pointers in Each Node".
+
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL
+    
+    not perfect full binary tree this time
+    
+    直接用多个指针来实现
+    dummy一直作为远程dummy
+    pre 一直往后连
+    root 一直层序往后
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode dummy = new TreeLinkNode(-1);
+        TreeLinkNode pre = dummy;
+        while (root != null) {
+            if (root.left != null) {
+                pre.next = root.left;
+                pre = pre.next;
+            }
+            if (root.right != null) {
+                pre.next = root.right;
+                pre = pre.next;
+            }
+            root = root.next;
+            if (root == null) {
+                pre = dummy;
+                root = dummy.next;
+                dummy.next = null;
+            }
+            
+        }
+    }
+
+}
