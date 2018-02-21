4e9b27fd68350a36e79251d57a9c01bb256bd34e
xiangji <xiang719@gmail.com>
2018-02-21 16:04:07 -0500

    u

---
+++ src/DSA/Array/PascalTriangle/Solution.java
@@ -0,0 +1,39 @@
+package DSA.Array.PascalTriangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+ */
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        if (numRows <=0){
+            return triangle;
+        }
+        for (int i=0; i<numRows; i++){
+            List<Integer> row =  new ArrayList<Integer>();
+            for (int j=0; j<i+1; j++){
+                if (j==0 || j==i){
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
