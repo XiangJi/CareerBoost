201dd969f1a7c23d6a6fdc80fd9b97d5761147a5
Xiang Ji <xiang719@gmail.com>
2018-02-22 23:26:04 -0500

    u

---
+++ src/DSA/Tree/FlattenBinaryTreetoLinkedList/Solution.java
@@ -0,0 +1,71 @@
+package DSA.Tree.FlattenBinaryTreetoLinkedList;
+
+import DSA.Tree.TreeNode;
+
+/*
+ * Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+click to show hints.
+
+Hints:
+If you notice carefully in the flattened tree, each node's right 
+child points to the next node of a pre-order traversal.
+
+
+ */
+class Solution {
+    
+    //post-order
+    private TreeNode prev;
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+            
+        prev = root;
+    }
+    
+    // preorder
+    private TreeNode lastNode = null;
+
+    public void flattenII(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (lastNode != null) {
+            lastNode.left = null;
+            lastNode.right = root;
+        }
+
+        lastNode = root;
+        TreeNode right = root.right;
+        flatten(root.left);
+        flatten(right);
+    }
+    
+}
