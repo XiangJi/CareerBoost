525c6abcabd9cf4483869731f4aaf845d73c40ef
Xiang Ji <xiang719@gmail.com>
2018-03-04 09:19:07 -0500

    u

--- src/DSA/BitManipulation/BitwiseANDofNumbersRange/Solution.java
+++ src/DSA/BitManipulation/BitwiseANDofNumbersRange/Solution.java
@@ -8,13 +8,12 @@ find the left common part, find the law
  */
 public class Solution {
     public int rangeBitwiseAnd(int m, int n) {
-    int c = 0;
-    while(m != n){
-        m >>= 1;
-        n >>= 1;
-        ++c;
-    }
-    return n << c;
+        int c = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            ++c;
+        }
+        return n << c;
     }
 }
-
