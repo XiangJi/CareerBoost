fe6aff5cf3b28ed97e39fa72e4610c6c5b799713
Xiang Ji <xiang719@gmail.com>
2018-03-07 10:15:15 -0500

    u

---
+++ src/DSA/Misc/WordBreak/Solution.java
@@ -0,0 +1,43 @@
+package DSA.Misc.WordBreak;
+
+import java.util.List;
+
+/*
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words. You may assume the dictionary does not contain duplicate words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+
+
+
+l e e t c o d e
+       i
+j 
+
+string.substring (j, i) mean index j to i - 1
+ */
+public class Solution {
+    
+    // DP
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+    
+    // Other solution:
+    // 1. brute force DFS
+    // 2. DFS with memoization
+    // 3. BFS
+}
