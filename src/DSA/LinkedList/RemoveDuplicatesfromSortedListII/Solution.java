02fc4b55f986cc38e22df2f7aa127c8b706f6fec
Xiang Ji <xiang719@gmail.com>
2018-03-11 16:55:50 -0400

    u

--- src/DSA/LinkedList/RemoveDuplicatesfromSortedListII/Solution.java
+++ src/DSA/LinkedList/RemoveDuplicatesfromSortedListII/Solution.java
@@ -2,31 +2,41 @@ package DSA.LinkedList.RemoveDuplicatesfromSortedListII;
 
 import DSA.LinkedList.ListNode;
 
+/*
+ * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+
+ */
+
 public class Solution {
     /**
      * @param ListNode head is the head of the linked list
      * @return: ListNode head of the linked list
      */
-    public static ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) {
-                return head;
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
         }
-
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        head = dummy;
-
-        while (head.next != null && head.next.next != null) {
-                if (head.next.val == head.next.next.val) {
-                        // do not know how many continuous dup nodes
-                        int val = head.next.val;
-                        while (head.next != null && head.next.val == val) {
-                                head.next = head.next.next; // delete one
-                        }
-                } else {
-                        head = head.next;
-                }
+        ListNode prev = dummy; // use prev pointer
+        ListNode current = head;
+        while (current != null) {
+            while (current.next != null && current.next.val == current.val) {
+                current = current.next;
+            }
+            
+            if (prev.next != current) {
+                prev.next = current.next;
+            } else {
+                prev = prev.next;
+            }
+            current = current.next;
         }
         return dummy.next;
     }
+    
 }
