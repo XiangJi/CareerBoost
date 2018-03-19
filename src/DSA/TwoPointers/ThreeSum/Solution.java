83ad3e81d87e3cd26b460f760c22a8b235e05ea2
xiangji <xiang719@gmail.com>
2018-03-19 15:43:23 -0400

    3 sum

---
+++ src/DSA/TwoPointers/ThreeSum/Solution.java
@@ -0,0 +1,15 @@
+package DSA.TwoPointers.ThreeSum;
+/*
+ * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+T: o(N ^ 2)
+S: o(N)
+Two pointers
+ */
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    }
+}
