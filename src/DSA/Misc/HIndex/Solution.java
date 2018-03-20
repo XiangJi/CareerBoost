a7535d19956c2e2b7927353ec6746b27de21453d
xiangji <xiang719@gmail.com>
2018-03-20 14:47:01 -0400

    u

--- src/DSA/Misc/HIndex/Solution.java
+++ src/DSA/Misc/HIndex/Solution.java
@@ -12,8 +12,13 @@ Note: If there are several possible values for h, the maximum one is taken as th
 画图 x：paper y citations
 问题就是找sort之后图中的最大方块
 两种方法来排序 比较排序和counting sort
-1. Sort
-After sorting, if citations[i] > i, paper 0 ~ i all has at least  i + 1 citations 
+1. Compare sort
+After sorting, if citations[i] > i, paper 0 ~ i all has at least  i + 1 citations
+
+2. Counting sort
+The most commonly used non-comparison sorting is counting sort
+Any citation larger than nn can be replaced by nn and the hh-index will not change after the replacement
+The reason is that hh-index is upper bounded by total number of papers
  */
 public class Solution {
     public int hIndex(int[] citations) {
@@ -27,5 +32,23 @@ public class Solution {
             i++;
         }
         return i;
+    }
+
+    // Counting sort
+    public int hIndexII(int[] citations) {
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // counting papers for each citation number
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // Now, we have the count number of papers for each citation number
+        // finding the h-index
+
+        int k = n;
+        // s means sum from the right
+        // the first k from right to left that has k<= s is the index
+        for (int s = papers[n]; k > s; s += papers[k])
+            k--;
+        return k;
     }
 }
