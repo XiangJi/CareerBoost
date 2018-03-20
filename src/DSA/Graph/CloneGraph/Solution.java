12809148ea635e2a476cec4f82bd41a0dc213c0f
xiangji <xiang719@gmail.com>
2018-03-20 16:29:32 -0400

    graph

---
+++ src/DSA/Graph/CloneGraph/Solution.java
@@ -0,0 +1,22 @@
+package DSA.Graph.CloneGraph;
+
+/*
+ * Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+ * G. F
+ *
+ * BFS, DFS
+ * T: O( E + V) V:Node E: Edge
+ * S: O(V)
+ */
+public class Solution {
+    // DFS
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode root) {
+
+    }
+
+    // BFS
+    public UndirectedGraphNode cloneGraphII(UndirectedGraphNode root) {
+
+    }
+
+}
