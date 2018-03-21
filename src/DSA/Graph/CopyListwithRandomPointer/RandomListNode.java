307f00681ca2717ed2fbff9c8b6a4519665f6ee2
xiangji <xiang719@gmail.com>
2018-03-21 10:33:19 -0400

    copy

---
+++ src/DSA/Graph/CopyListwithRandomPointer/RandomListNode.java
@@ -0,0 +1,10 @@
+package DSA.Graph.CopyListwithRandomPointer;
+
+public class RandomListNode {
+    int            label;
+    RandomListNode next, random;
+
+    RandomListNode(int x) {
+        this.label = x;
+    }
+}
