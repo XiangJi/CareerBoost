70657e7620df4264b2ec29d323696ac56c9d9d9d
xiangji <xiang719@gmail.com>
2018-03-23 17:34:13 -0400

    u

---
+++ src/DSA/DynamicProgramming/PerfectSquares/Solution.java
@@ -0,0 +1,11 @@
+package DSA.DynamicProgramming.PerfectSquares;
+
+/*
+ * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+DP 问题的风格 想推倒公式
+ */
+public class Solution {
+
+}
