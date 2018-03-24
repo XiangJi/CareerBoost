60b3288e1d524bb9a4d0b99751b7651f119ef4b3
Xiang Ji <xiang719@gmail.com>
2018-03-24 10:14:26 -0400

    u

--- src/DSA/DynamicProgramming/PerfectSquares/Solution.java
+++ src/DSA/DynamicProgramming/PerfectSquares/Solution.java
@@ -1,11 +1,26 @@
 package DSA.DynamicProgramming.PerfectSquares;
 
+import java.util.Arrays;
+
 /*
  * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
 For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
 DP 问题的风格 想推倒公式
+Arrays.fill(array, num);
+Implementation trick, j * j <= i (not n!)
+T(n sqrt root n)
  */
 public class Solution {
-
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
 }
