f8e10a71ccfe443bc8f1d05f56aeee7838bada26
Xiang Ji <xiang719@gmail.com>
2018-04-09 21:42:51 -0400

    u

--- src/DSA/BinarySearch/Searcha2DMatrixII/Solution.java
+++ src/DSA/BinarySearch/Searcha2DMatrixII/Solution.java
@@ -6,27 +6,51 @@ Integers in each row are sorted in ascending from left to right.
 Integers in each column are sorted in ascending from top to bottom.
 
 
-search from the top right
+search from the left bottom to right top
+O(N+M) solution:Search Space Reduction prune
+
  */
 
 public class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-            return false;
+    /**
+     * @param matrix: A list of lists of integers
+     * @param: A number you want to search in the matrix
+     * @return: An integer indicate the occurrence of target in the given matrix
+     */
+    public int searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        int  i = matrix.length - 1;
+        int j = 0;
+        int ans = 0;
+        while ( i >= 0 && j < matrix[0].length) {
+            if (target == matrix[i][j]) {
+                ans++;
+                i--;
+                j++;
+                continue;
+            }
+            if (target < matrix[i][j]) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
+    
+    public boolean searchMatrixLeetCode(int[][] matrix, int target) {
+        // start our "pointer" in the bottom-left
+        int row = matrix.length-1;
+        int col = 0;
 
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int i = 0, j = n - 1;
-
-        while (i < m && j >= 0) {
-            int num = matrix[i][j];
-
-            if (num == target)
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else { // found it
                 return true;
-            else if (num > target)
-                j--;
-            else
-                i++;
+            }
         }
 
         return false;
