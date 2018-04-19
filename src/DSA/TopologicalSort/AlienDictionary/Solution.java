8d91809eb95a7858af62db201f71dd089d3c5a5f
xiangji <xiang719@gmail.com>
2018-04-18 20:36:34 -0400

    u

---
+++ src/DSA/TopologicalSort/AlienDictionary/Solution.java
@@ -0,0 +1,115 @@
+package DSA.TopologicalSort.AlienDictionary;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/*
+ * There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+
+Example 1:
+Given the following words in dictionary,
+
+[
+  "wrt",
+  "wrf",
+  "er",
+  "ett",
+  "rftt"
+]
+The correct order is: "wertf".
+
+Example 2:
+Given the following words in dictionary,
+
+[
+  "z",
+  "x"
+]
+The correct order is: "zx".
+
+Example 3:
+Given the following words in dictionary,
+
+[
+  "z",
+  "x",
+  "z"
+]
+The order is invalid, so return "".
+
+Note:
+You may assume all letters are in lowercase.
+You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+If the order is invalid, return an empty string.
+There may be multiple valid order of letters, return any one of them is fine.
+
+拓扑排序
+BFS
+
+Construct graph, find indegree 1, BFS
+Idea, 两两比较找？
+
+ */
+class Solution {
+    public static String alienOrder(String[] words) {
+
+        if (words == null || words.length == 0) return "";
+
+        StringBuilder res = new StringBuilder();
+        HashMap<Character, Set<Character>> map = new HashMap<>();
+        int[] degree = new int[26];
+        int count = 0;
+
+        // count 找到了所有独一无二字母，一开始把所有入度变成1
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                if (degree[c - 'a'] == 0) {
+                    count++;
+                    degree[c - 'a'] = 1;
+                }
+            }
+        }
+        // 词和词两两比较，构图
+        for (int i = 0; i < words.length - 1; i++) {
+            char[] cur = words[i].toCharArray();
+            char[] next = words[i + 1].toCharArray();
+            int len = Math.min(cur.length, next.length);
+            for (int j = 0; j < len; j++) {
+                if (cur[j] != next[j]) {
+                    if (!map.containsKey(cur[j])) {
+                        map.put(cur[j], new HashSet<>());
+                    }
+                    if (map.get(cur[j]).add(next[j])) {
+                        degree[next[j] - 'a']++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        Queue<Character> queue = new LinkedList<>();
+        for (int i = 0; i < 26; i++) {
+            if (degree[i] == 1) {
+                queue.offer((char)('a' + i));
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            char c = queue.poll();
+            res.append(c);
+            if (map.containsKey(c)) {
+                for (char ch : map.get(c)) {
+                    if (--degree[ch - 'a'] == 1) {
+                        queue.offer(ch);
+                    }
+                }
+            }
+        }
+
+        if (res.length() != count) return "";
+        return res.toString();
+    }
+}
