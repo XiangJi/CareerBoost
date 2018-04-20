d142ce5f8d79b8e421ec8b23502268b19057e7de
Xiang Ji <xiang719@gmail.com>
2018-04-19 20:43:54 -0400

    u

--- src/DSA/Misc/SentenceSimilarityII/Solution.java
+++ src/DSA/Misc/SentenceSimilarityII/Solution.java
@@ -3,49 +3,29 @@ package DSA.Misc.SentenceSimilarityII;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-    public boolean areSentencesSimilarTwo(String[] words1, String[] words2, String[][] pairs) {
-        if (words1.length != words2.length)
-            return false;
-        Map<String, Integer> index = new HashMap();
-        int count = 0;
-        DSU dsu = new DSU(2 * pairs.length);
-        for (String[] pair : pairs) {
-            for (String p : pair)
-                if (!index.containsKey(p)) {
-                    index.put(p, count++);
-                }
-            dsu.union(index.get(pair[0]), index.get(pair[1]));
+/*
+ * This is a good use case for Union-Find, compare to Sentence Similarity I, here the similarity between words are transitive, so all the connected(similar) words should be group into an union represented by their ultimate parent(or family holder, you name it).
+The connections can be represented by an parent map Map<String, String> m, which record the direct parent-ship we learned in each pair, but not the ultimate-parent. To build it, go through the input pairs, for each pair<w1, w2>, use the recursive find() method to find the ultimate-parent for both word - parent1, parent2, if they are different, assign parent1 as parent of parent2(or the other way around), so that the to families are merged.
+The classic find(x) method will find the ultimate-parent of x. I modified it a little bit, make it do a little of extra initialization work - assign x itself as its parent when it is not initialize - so that we don’t have to explicitly initialize the map at the beginning.
+
+ */
+class Solution {
+    public boolean areSentencesSimilarTwo(String[] a, String[] b, String[][] pairs) {
+        if (a.length != b.length) return false;
+        Map<String, String> m = new HashMap<>();
+        for (String[] p : pairs) {
+            String parent1 = find(m, p[0]), parent2 = find(m, p[1]);
+            if (!parent1.equals(parent2)) m.put(parent1, parent2);
         }
 
-        for (int i = 0; i < words1.length; ++i) {
-            String w1 = words1[i], w2 = words2[i];
-            if (w1.equals(w2))
-                continue;
-            if (!index.containsKey(w1) || !index.containsKey(w2) || dsu.find(index.get(w1)) != dsu.find(index.get(w2)))
-                return false;
-        }
+        for (int i = 0; i < a.length; i++)
+            if (!a[i].equals(b[i]) && !find(m, a[i]).equals(find(m, b[i]))) return false;
+
         return true;
     }
-}
 
-class DSU {
-    int[] parent;
-
-    // init
-    public DSU(int N) {
-        parent = new int[N];
-        for (int i = 0; i < N; ++i)
-            parent[i] = i;
-    }
-    // find
-    public int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    //union
-    public void union(int x, int y) {
-        parent[find(x)] = find(y);
+    private String find(Map<String, String> m, String s) {
+        if (!m.containsKey(s)) m.put(s, s);
+        return s.equals(m.get(s)) ? s : find(m, m.get(s));
     }
 }
