987d67fa0e97374fe339110000bc91df9c62717b
xiangji <xiang719@gmail.com>
2018-04-27 16:16:09 -0400

    U

---
+++ src/DSA/Misc/MyCalendarII/MyCalendarTwo.java
@@ -0,0 +1,59 @@
+package DSA.Misc.MyCalendarII;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+/*
+ * 不能有triple
+ * store all the booking and overlaps
+ *
+ * 1. BF
+ * 2. Boundary count
+ */
+public class MyCalendarTwo {
+    List<int[]> calendar;
+    List<int[]> overlaps;
+
+    MyCalendarTwo() {
+        calendar = new ArrayList();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] iv: overlaps) {
+            if (iv[0] < end && start < iv[1]) return false;
+        }
+        for (int[] iv: calendar) {
+            if (iv[0] < end && start < iv[1])
+                overlaps.add(new int[]{Math.max(start, iv[0]), Math.min(end, iv[1])});
+        }
+        calendar.add(new int[]{start, end});
+        return true;
+    }
+}
+//Boundary count 扫描线算法 入++ 出-- 如果到3就不行了
+class MyCalendarTwoII {
+    TreeMap<Integer, Integer> delta;
+
+    public MyCalendarTwoII() {
+        delta = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+        delta.put(start, delta.getOrDefault(start, 0) + 1);
+        delta.put(end, delta.getOrDefault(end, 0) - 1);
+
+        int active = 0;
+        for (int d: delta.values()) {
+            active += d;
+            if (active >= 3) {
+                delta.put(start, delta.get(start) - 1);
+                delta.put(end, delta.get(end) + 1);
+                if (delta.get(start) == 0)
+                    delta.remove(start);
+                return false;
+            }
+        }
+        return true;
+    }
+}
