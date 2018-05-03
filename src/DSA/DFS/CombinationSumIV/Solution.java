55a872342393bddd687d953d8b1c7e154e7bf813
xiangji <xiang719@gmail.com>
2018-05-03 10:42:29 -0400

    u

--- src/DSA/DFS/CombinationSumIV/Solution.java
+++ src/DSA/DFS/CombinationSumIV/Solution.java
@@ -33,6 +33,15 @@ Counter DP
  */
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-
+        int[] dp = new int[target + 1];
+        dp[0] = 1; // 1 way
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (i - num >= 0) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
     }
 }
