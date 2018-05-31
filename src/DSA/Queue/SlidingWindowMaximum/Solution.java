e2e0e79be73f28776e28cd2b7784af185f189fef
xiangji <xiang719@gmail.com>
2018-05-31 13:19:30 -0400

    a

---
+++ src/DSA/Queue/SlidingWindowMaximum/Solution.java
@@ -0,0 +1,65 @@
+package DSA.Queue.SlidingWindowMaximum;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7]
+Explanation:
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Note:
+You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+
+BF: o((n - k +1) * k) O(1)
+BST/Heap: o((n - k +1) * logk) o(k) to store
+Deque: o(n) o(k), 单调栈 但是java用双端队列
+
+offer: will pop all the elements smaller then E
+poll: pop the max one in the head
+peek: get the max
+ */
+public class Solution {
+    public int[] maxSlidingWindow(int[] a, int k) {
+        if (a == null || k <= 0) {
+            return new int[0];
+        }
+        int n = a.length;
+        int[] result = new int[n-k+1];
+        int ri = 0;
+        // store index
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < a.length; i++) {
+            // remove numbers out of range k
+            while (!q.isEmpty() && q.peek() < i - k + 1) {
+                q.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            // just change the boolean here
+            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+                q.pollLast();
+            }
+            // q contains index... r contains content
+            q.offer(i);
+            if (i >= k - 1) {
+                result[ri++] = a[q.peek()];
+            }
+        }
+        return result;
+    }
+}
