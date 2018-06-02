aae7eb5c1920a825e66d2ee8c21a60129739314e
Xiang Ji <xiang719@gmail.com>
2018-06-02 16:59:32 -0400

    u

--- src/DSA/Greedy/PatchingArray/Solution.java
+++ src/DSA/Greedy/PatchingArray/Solution.java
@@ -21,9 +21,50 @@ Example 3:
 Input: nums = [1,2,2], n = 5
 Output: 0
 
+Suppose the number we added is xx then, the ranges [1, miss) and [x, x + miss) are both covered. And since we know that x <= miss, the two ranges will cover the range [1, x + miss). We want to choose xx as large as possible so that the range can cover as large as possible. Therefore, the best option is x = miss.
+
+After we covered miss, we can recalculate the coverage and see what's the new smallest missing number. We then patch that number. We do this repeatedly until no missing number.
+
+Here is the recipe of this greedy algorithm:
+
+Initialize the range [1, miss) = [1, 1) = empty
+While n is not covered yet
+if the current element nums[i] is less than or equal to miss
+extends the range to [1, miss + nums[i])
+increase i by 1
+otherwise
+patch the array with miss, extends the range to [1, miss + miss)
+increase the number of patches
+Return the number of patches
+ 
+这道题给我们一个有序的正数数组nums，又给了我们一个正整数n，问我们最少需要给nums加几个数字，
+使其能组成[1,n]之间的所有数字，注意数组中的元素不能重复使用，否则的话只有要有1，就能组成所有的
+数字了。这道题我又不会了，上网看到了史蒂芬大神的解法，膜拜啊，这里就全部按他的解法来讲吧。我们定义一个变量miss，用来表示
+[0,n]之间最小的不能表示的值，那么初始化为1，为啥不为0呢，因为n=0没啥意义，直接返回0了。那么此时我们能表示的范围是[0, miss)，
+表示此时我们能表示0到miss-1的数，如果此时的num <= miss，那么我们可以把我们能表示数的范围扩大到[0, miss+num)，如果num>miss，
+那么此时我们需要添加一个数，为了能最大限度的增加表示数范围，我们加上miss它本身，以此类推直至遍历完整个数组，我们可
+以得到结果。下面我们来举个例子说明：
+给定nums = [1, 2, 4, 11, 30], n = 50，我们需要让[0, 50]之间所有的数字都能被nums中的数
+字之和表示出来。
+首先使用1, 2, 4可能表示出0到7之间的所有数，表示范围为[0, 8)，但我们不能表示8，因为下一个数字11太大了，
+所以我们要在数组里加上一个8，此时能表示的范围是[0, 16)，那么我们需要插入16吗，答案是不需要，因为我们数组有1和4
+，可以组成5，而下一个数字11，加一起能组成16，所以有了数组中的11，我们此时能表示的范围扩大到[0, 27)，但我们没法表示27，
+因为30太大了，所以此时我们给数组中加入一个27，那么现在能表示的范围是[0, 54)，已经满足要求了，我们总共添加了两个数8和27，所
+以返回2即可。
+
  */
-class Solution {
+public class Solution {
     public int minPatches(int[] nums, int n) {
-        
+        int patches = 0, i = 0;
+        long miss = 1; // use long to avoid integer overflow error
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) // miss is covered
+                miss += nums[i++];
+            else { // patch miss to the array
+                miss += miss;
+                patches++; // increase the answer
+            }
+        }
+        return patches;
     }
 }
