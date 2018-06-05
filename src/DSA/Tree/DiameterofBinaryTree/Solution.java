8341e5e54375229b46548b8077856f5665fcd75d
Xiang Ji <xiang719@gmail.com>
2018-06-04 22:00:00 -0400

    u

---
+++ src/DSA/Tree/DiameterofBinaryTree/Solution.java
@@ -0,0 +1,38 @@
+package DSA.Tree.DiameterofBinaryTree;
+
+import DSA.Tree.TreeNode;
+
+/*
+ * Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+Example:
+Given a binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+ */
+public class Solution {
+    int res;
+    public int diameterOfBinaryTree(TreeNode root) {
+        res = 0;
+        longestPath(root);
+        return res;
+    }
+    // return the max depth of one node
+    public int longestPath(TreeNode current) {
+        if (current == null) {
+            return 0;
+        }
+        int left = longestPath(current.left);
+        int right = longestPath(current.right);
+
+        res = Math.max(res, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
---
+++ src/DSA/Tree/LongestUnivaluePath/Solution.java
@@ -0,0 +1,64 @@
+package DSA.Tree.LongestUnivaluePath;
+
+import DSA.Tree.TreeNode;
+
+/*
+ * Given a binary tree, find the length of the longest path where each node in the path has the same value. This path may or may not pass through the root.
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+Example 1:
+
+Input:
+
+              5
+             / \
+            4   5
+           / \   \
+          1   1   5
+Output:
+
+2
+Example 2:
+
+Input:
+
+              1
+             / \
+            4   5
+           / \   \
+          4   4   5
+Output:
+
+2
+
+
+ */
+public class Solution {
+    int res;
+
+    public int longestUnivaluePath(TreeNode root) {
+        res = 0;
+        pathLength(root);
+        return res;
+    }
+
+    // find the one side max univalue path
+    public int pathLength(TreeNode current) {
+        if (current == null) {
+            return 0;
+        }
+        int left = pathLength(current.left);
+        int right = pathLength(current.right);
+        int l = 0;
+        int r = 0;
+        if (current.left != null && current.left.val == current.val) {
+            l = left + 1;
+        }
+        if (current.right != null && current.val == current.right.val) {
+            r = right + 1;
+        }
+        res = Math.max(res, l + r);
+        return Math.max(l, r);
+    }
+}
