fa4d576dccef52f7d28cbc4bc80f31fc57623b87
Xiang Ji <xiang719@gmail.com>
2018-06-10 23:50:42 -0400

    u

--- src/DSA/Tree/DiameterofBinaryTree/Solution.java
+++ src/DSA/Tree/DiameterofBinaryTree/Solution.java
@@ -3,7 +3,9 @@ package DSA.Tree.DiameterofBinaryTree;
 import DSA.Tree.TreeNode;
 
 /*
- * Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+ * Given a binary tree, you need to compute the length of the diameter of the tree. T
+ * he diameter of a binary tree is the length of the longest path between any two nodes
+ *  in a tree. This path may or may not pass through the root.
 
 Example:
 Given a binary tree 
