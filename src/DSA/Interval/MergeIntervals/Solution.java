4a4250ee57dddbb59adca2b8ca5928b22a128e0a
xiangji <xiang719@gmail.com>
2018-06-13 16:05:59 -0400

    u

--- src/DSA/Interval/MergeIntervals/Solution.java
+++ src/DSA/Interval/MergeIntervals/Solution.java
@@ -19,7 +19,9 @@ return [1,6],[8,10],[15,18].
  *     Interval() { start = 0; end = 0; }
  *     Interval(int s, int e) { start = s; end = e; }
  * }
- */
+ *
+ *
+ * */
 class Solution {
 
     // basic algorithm: if overlap, merge, if not add the prev one
@@ -29,7 +31,7 @@ class Solution {
 
         intervals.sort(Comparator.comparing(i -> i.start));  //lambda 匿名函数：输入i  返回i.start
 
-        /*
+        /* 熟练写comparator!
          * Collections.sort(intervals, new Comparator<Interval>() {
                 @Override
                 public int compare(Interval i1, Interval i2) {
@@ -37,13 +39,14 @@ class Solution {
                 }
         });
          */
+        // Use last pointer
         Interval last = null;
         for (Interval item : intervals) {
             if (last == null || last.end < item.start) {
                 ans.add(item);
                 last = item;
             } else {
-                last.end = Math.max(last.end, item.end); // Modify the element already in list
+                last.end = Math.max(last.end, item.end); // Modify the element already in list, last可以修改进res的区间
             }
         }
         return ans;
