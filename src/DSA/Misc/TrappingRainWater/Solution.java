13ededf2a9780e2b907f864eb2dc5f37288ee198
Xiang Ji <xiang719@gmail.com>
2018-06-13 22:17:00 -0400

    u

--- src/DSA/Misc/TrappingRainWater/Solution.java
+++ src/DSA/Misc/TrappingRainWater/Solution.java
@@ -1,4 +1,5 @@
 package DSA.Misc.TrappingRainWater;
+
 /*
  * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
  * 
@@ -11,26 +12,30 @@ Output: 6
 public class Solution {
     // DP 存left 高 right 高 然后取min -自己高就是每个位置的高度
     public int trap(int[] height) {
-        if(height == null)
+        if (height == null || height.length == 0)
             return 0;
         int ans = 0;
         int size = height.length;
-        int[] left_max = new int[size], right_max = new int[size];
+        int[] left_max = new int[size],
+                right_max = new int[size];
         // init
         left_max[0] = height[0];
         for (int i = 1; i < size; i++) {
-            left_max[i] = Math.max(height[i], left_max[i - 1]);
+            left_max[i] = Math.max(height[i],
+                    left_max[i - 1]);
         }
         // init
         right_max[size - 1] = height[size - 1];
         for (int i = size - 2; i >= 0; i--) {
-            right_max[i] = Math.max(height[i], right_max[i + 1]);
+            right_max[i] = Math.max(height[i],
+                    right_max[i + 1]);
         }
         for (int i = 1; i < size - 1; i++) {
-            ans += Math.min(left_max[i], right_max[i]) - height[i];
+            ans += Math.min(left_max[i], right_max[i])
+                    - height[i];
         }
         return ans;
     }
-    
+
     // 优化 双指针
 }
