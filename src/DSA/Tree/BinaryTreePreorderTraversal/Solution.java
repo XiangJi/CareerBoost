e9fb558fdeeb60db5b5612f9a71e9ed9f65b5e21
Xiang Ji <xiang719@gmail.com>
2018-06-15 20:07:33 -0400

    u

---
+++ src/DSA/Tree/BinaryTreePreorderTraversal/Solution.java
@@ -0,0 +1,49 @@
+package DSA.Tree.BinaryTreePreorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import DSA.Tree.TreeNode;
+
+/*
+ * 确实有些题确实需要背诵 模板题目
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        // Divide
+        List<Integer> left = preorderTraversal(root.left);
+        List<Integer> right = preorderTraversal(root.right);
+        
+        
+        //Conquer
+        result.add(root.val);
+        result.addAll(left);
+        result.addAll(right);
+        
+        return result;
+    }
+    
+    // iterative stack
+    public List<Integer> preorder(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur.right != null) stack.push(cur.right);
+            if (cur.left != null) stack.push(cur.left); 
+            res.add(cur.val);
+        }
+        return res;
+    }
+}
