9f2b5ca0b79cc89f54fd415e0d8ad296e7869ec7
Xiang Ji <xiang719@gmail.com>
2018-06-16 17:37:50 -0400

    U

--- src/DSA/Tree/ClosestBinarySearchTreeValue/Solution.java
+++ src/DSA/Tree/ClosestBinarySearchTreeValue/Solution.java
@@ -2,6 +2,9 @@ package DSA.Tree.ClosestBinarySearchTreeValue;
 
 import DSA.Tree.TreeNode;
 
+//inorder traversal
+// both recursive and iterative
+
 class Solution {
     public int closestValue(TreeNode root, double target) {
         TreeNode current = root;
@@ -13,5 +16,13 @@ class Solution {
         }
         
         return prev;
+    }
+    
+    public int closestValueII(TreeNode root, double target) {
+        int a = root.val;
+        TreeNode kid = target < a ? root.left : root.right;
+        if (kid == null) return a;
+        int b = closestValue(kid, target);
+        return Math.abs(a - target) < Math.abs(b - target) ? a : b;
     }
 }
