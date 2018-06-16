f8a5e87783a5542baa9d8c7a9777b0bbfbea3189
Xiang Ji <xiang719@gmail.com>
2018-06-16 18:22:56 -0400

    u

--- src/DSA/TwoPointers/MoveZeroes/Solution.java
+++ src/DSA/TwoPointers/MoveZeroes/Solution.java
@@ -4,12 +4,16 @@ package DSA.TwoPointers.MoveZeroes;
  * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative
  * order of the non-zero elements.
  * For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+ * 
+ *     *All elements before the slow pointer (lastNonZeroFoundAt) are non-zeroes.
+    *All elements between the current and slow pointer are zeroes.
+    *
  */
 public class Solution {
     public void moveZeroes(int[] nums) {
         // use a pointer to save index, swap 0 and number
         // O(2*non-zero)
-        int index = 0;
+        int index = 0; // slow pointer index to keep the firstZero
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 // swap with the first zero
