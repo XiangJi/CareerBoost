bd64f25d391df9d5a24c7f065db22b1bcecfcded
Xiang Ji <xiang719@gmail.com>
2018-06-17 16:44:37 -0400

    u

---
+++ src/DSA/BFS/ShortestDistancefromAllBuildings/Solution.java
@@ -0,0 +1,102 @@
+package DSA.BFS.ShortestDistancefromAllBuildings;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * You want to build a house on an empty land which reaches all buildings in the shortest amount of distance. You can only move up, down, left and right. You are given a 2D grid of values 0, 1 or 2, where:
+
+Each 0 marks an empty land which you can pass by freely.
+Each 1 marks a building which you cannot pass through.
+Each 2 marks an obstacle which you cannot pass through.
+For example, given three buildings at (0,0), (0,4), (2,2), and an obstacle at (0,2):
+
+1 - 0 - 2 - 0 - 1
+|   |   |   |   |
+0 - 0 - 0 - 0 - 0
+|   |   |   |   |
+0 - 0 - 1 - 0 - 0
+The point (1,2) is an ideal empty land to build a house, as the total travel distance of 3+3+1=7 is minimal. So return 7.
+
+Note:
+There will be at least one building. If it is not possible to build such house according to the above rules, return -1.
+
+这题就和之前那道Walls and Gates很类似
+要原因还是由于DFS的搜索方式不适合距离场，因为BFS遍历完一个点后，不会再来更改这个点的值，
+我们对于每一个建筑的位置都进行一次全图的BFS遍历，dist[][] 
+Traverse the matrix. For each building, use BFS to compute the shortest distance from each '0' to
+this building. After we do this for all the buildings, we can get the sum of shortest distance
+from every '0' to all reachable buildings. This value is stored
+in 'distance[][]'.
+
+
+
+int[][] dist
+     int[][] nums
+     for
+        for
+            if (grid[i][j] == '1')
+               BFS
+     * time: O(m^2 * n^2)
+     * space: O(mn)
+     * 
+ */
+public class Solution {
+    public int shortestDistance(int[][] grid) {
+        if (grid == null || grid.length == 0) return -1;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dist = new int[m][n];
+        int[][] nums = new int[m][n];
+        int buildingNum = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    buildingNum++;
+                    bfs(grid, i, j, dist, nums);
+                }
+            }
+        }
+        // cnt表示某个位置已经计算过的建筑数，变量buildingCnt为建筑的总数
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0 && dist[i][j] != 0 && nums[i][j] == buildingNum) {
+                    res = Math.min(res, dist[i][j]);
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    private void bfs(int[][] grid, int row, int col, int[][] dist, int[][] nums) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{row, col});
+
+        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        boolean[][] visited = new boolean[m][n];
+        int distance = 0;
+
+        while (!queue.isEmpty()) {
+            distance++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                for (int k = 0; k < dirs.length; k++) {
+                    int x = cur[0] + dirs[k][0];
+                    int y = cur[1] + dirs[k][1];
+                    if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y] && grid[x][y] == 0) {
+                        visited[x][y] = true;
+                        dist[x][y] += distance;
+                        nums[x][y]++;
+                        queue.offer(new int[]{x, y});
+                    }
+                }
+            }
+        }
+
+
+    }
+}
