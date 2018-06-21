af372693ed4f1b8b9e79a5a377364114cadd4beb
xiangji <xiang719@gmail.com>
2018-06-21 18:59:22 -0400

    u

---
+++ src/DSA/Iterator/DesignCompressedStringIterator/StringIterator.java
@@ -0,0 +1,60 @@
+package DSA.Iterator.DesignCompressedStringIterator;
+/*
+ * Design and implement a data structure for a compressed string iterator. It should support the following operations: next and hasNext.
+
+The given compressed string will be in the form of each letter followed by a positive integer representing the number of this letter existing in the original uncompressed string.
+
+next() - if the original string still has uncompressed characters, return the next letter; Otherwise return a white space.
+hasNext() - Judge whether there is any letter needs to be uncompressed.
+
+Note:
+Please remember to RESET your class variables declared in StringIterator, as static/class variables are persisted across multiple test cases. Please see here for more details.
+
+Example:
+
+StringIterator iterator = new StringIterator("L1e2t1C1o1d1e1");
+
+iterator.next(); // return 'L'
+iterator.next(); // return 'e'
+iterator.next(); // return 'e'
+iterator.next(); // return 't'
+iterator.next(); // return 'C'
+iterator.next(); // return 'o'
+iterator.next(); // return 'd'
+iterator.hasNext(); // return true
+iterator.next(); // return 'e'
+iterator.hasNext(); // return false
+iterator.next(); // return ' '
+
+Since no precomputation is done, constant space is required in this case.
+
+The time required to perform next() operation is O(1)O(1).
+
+The time required for hasNext() operation is O(1)O(1).
+
+Since no precomputations are done, and hasNext() requires only O(1)O(1) time, this solution is advantageous if hasNext() operation is performed most of the times.
+
+ */
+public class StringIterator {
+    String res;
+    int ptr = 0, num = 0;
+    char ch = ' ';
+    public StringIterator(String s) {
+        res = s;
+    }
+    public char next() {
+        if (!hasNext())
+            return ' ';
+        if (num == 0) {
+            ch = res.charAt(ptr++);
+            while (ptr < res.length() && Character.isDigit(res.charAt(ptr))) {
+                num = num * 10 + res.charAt(ptr++) - '0';
+            }
+        }
+        num--;
+        return ch;
+    }
+    public boolean hasNext() {
+        return ptr != res.length() || num != 0;
+    }
+}
--- src/DSA/Iterator/PeekingIterator/PeekingIterator.java
+++ src/DSA/Iterator/PeekingIterator/PeekingIterator.java
@@ -1,4 +1,5 @@
 package DSA.Iterator.PeekingIterator;
+import java.util.ArrayList;
 /*
  * iven an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator that support the peek() operation -- it essentially peek() at the element that will be returned by the next call to next().
 
@@ -7,8 +8,8 @@ Example:
 Assume that the iterator is initialized to the beginning of the list: [1,2,3].
 
 Call next() gets you 1, the first element in the list.
-Now you call peek() and it returns 2, the next element. Calling next() after that still return 2. 
-You call next() the final time and it returns 3, the last element. 
+Now you call peek() and it returns 2, the next element. Calling next() after that still return 2.
+You call next() the final time and it returns 3, the last element.
 Calling hasNext() after that should return false.
 Follow up: How would you extend your design to be generic and work with all types, not just integer?
  */
@@ -43,4 +44,36 @@ public class PeekingIterator implements Iterator<Integer> {
     public boolean hasNext() {
         return head != null;
     }
-}    
+}
+
+class StringIterator {
+    ArrayList<Character> list;
+    Iterator<Character> iter;
+    public StringIterator(String compressedString) {
+        list = new ArrayList<>();
+        int i = 0;
+        while (i < compressedString.length()) {
+            char c = compressedString.charAt(i);
+            int j = i + 1;
+            int n = 0;
+            while (Character.isDigit(compressedString.charAt(j))){
+                n = 10 * n + (compressedString.charAt(j) - '0');
+            }
+
+            while (n > 0) {
+                list.add(c);
+                n--;
+            }
+            i = j - 1;
+        }
+        iter = list.iterator();
+    }
+
+    public char next() {
+        return iter.next();
+    }
+
+    public boolean hasNext() {
+        return iter.hasNext();
+    }
+}
