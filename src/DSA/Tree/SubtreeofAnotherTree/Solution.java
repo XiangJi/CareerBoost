d12d01d967047fe04dca5d2cca8c14e71f837877
xiangji <xiang719@gmail.com>
2018-06-22 13:44:49 -0400

    u

---
+++ src/DSA/Tree/SubtreeofAnotherTree/Solution.java
@@ -0,0 +1,69 @@
+package DSA.Tree.SubtreeofAnotherTree;
+
+import DSA.Tree.TreeNode;
+
+/*
+ * Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+
+preorder遍历树 看每个节点是不是和t是same tree
+
+o(m * n)
+
+s: o(n)
+ */
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        if (isSameTree(s,t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+
+        if (p.val == q.val && left && right) {
+            return true;
+        }
+        return false;
+    }
+}
