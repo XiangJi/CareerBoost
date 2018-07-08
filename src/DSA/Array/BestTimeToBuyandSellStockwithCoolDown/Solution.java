b38ca9fbf73b2c884751873ae5ecee4703c6151d
Xiang Ji <xiang719@gmail.com>
2018-07-07 22:52:28 -0400

    u

--- src/DSA/Array/BestTimeToBuyandSellStockwithCoolDown/Solution.java
+++ src/DSA/Array/BestTimeToBuyandSellStockwithCoolDown/Solution.java
@@ -1,6 +1,37 @@
 package DSA.Array.BestTimeToBuyandSellStockwithCoolDown;
 
-// ?
-public class Solution {
+/*
+ * Say you have an array for which the ith element is the price of a given stock on day i.
 
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+Example:
+
+Input: [1,2,3,0,2]
+Output: 3 
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+To represent the decision at index i:
+
+buy[i]: Max profit till index i. The series of transaction is ending with a buy.
+sell[i]: Max profit till index i. The series of transaction is ending with a sell.
+To clarify:
+
+Till index i, the buy / sell action must happen and must be the last action. It may not happen at index i. It may happen at i - 1, i - 2, ... 0.
+In the end n - 1, return sell[n - 1]. Apparently we cannot finally end up with a buy. In that case, we would rather take a rest at n - 1.
+For special case no transaction at all, classify it as sell[i], so that in the end, we can still return sell[n - 1].
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
+        for (int price : prices) {
+            prev_buy = buy;
+            buy = Math.max(prev_sell - price, prev_buy);
+            prev_sell = sell;
+            sell = Math.max(prev_buy + price, prev_sell);
+        }
+        return sell;
+    }
 }
