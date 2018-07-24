6713924f816b5339e20c1b7618308486f161edf9
xiangji <xiang719@gmail.com>
2018-07-24 14:40:43 -0400

    u

--- src/DSA/BinaryIndexedTree/RangeSumQuery2DImmutable/NumMatrix.java
+++ src/DSA/BinaryIndexedTree/RangeSumQuery2DImmutable/NumMatrix.java
@@ -1,4 +1,4 @@
-package DSA.BinaryIndexTree.RangeSumQuery2DImmutable;
+package DSA.BinaryIndexedTree.RangeSumQuery2DImmutable;
 /*
  * Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
 
--- src/DSA/BinaryIndexedTree/RangeSumQueryImmutable/NumArray.java
+++ src/DSA/BinaryIndexedTree/RangeSumQueryImmutable/NumArray.java
@@ -1,4 +1,4 @@
-package DSA.BinaryIndexTree.RangeSumQueryImmutable;
+package DSA.BinaryIndexedTree.RangeSumQueryImmutable;
 
 /*
  * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
