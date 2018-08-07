5a2bde7e44fc2ec43be4af97abcb3c81f6febc22
xiangji <xiang719@gmail.com>
2018-08-07 14:20:16 -0400

    u

---
+++ src/DSA/Iterator/ExpandIterator/Solution.java
@@ -0,0 +1,10 @@
+package DSA.Iterator.ExpandIterator;
+/*
+ *  给个array,比如[2,3,4,10...] 每两个两个看， 代表3出现两次，10出现4次。要求实现NEXT()和HASNEXT()。
+ *  用个pointer和counter记录一下就好了啊，注意corner case。follow up: 给的不是array,是个iterator.
+ *  BF: precompute, space too large
+ *  Use pointer and count
+ */
+public class Solution {
+
+}
