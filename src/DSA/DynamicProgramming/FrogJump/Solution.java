6eeb2223a98c28fb52d124ad05b9f5b63161acaa
xiangji <xiang719@gmail.com>
2018-08-15 10:34:45 -0400

    u

--- src/DSA/DynamicProgramming/FrogJump/Solution.java
+++ src/DSA/DynamicProgramming/FrogJump/Solution.java
@@ -19,16 +19,17 @@ The first stone's position is always 0.
  */
 public class Solution {
     public boolean canCross(int[] stones) {
-        HashMap<Integer, Set<Integer>> map = new HashMap<>();
+        HashMap<Integer, Set<Integer>> map = new HashMap<>();// store position and last junmp can reach here
         for (int i = 0; i < stones.length; i++) {
             map.put(stones[i], new HashSet<Integer>());
         }
         map.get(0).add(0);
         for (int i = 0; i < stones.length; i++) {
-            for (int k : map.get(stones[i])) {
-                for (int step = k - 1; step <= k + 1; step++) {
-                    if (step > 0 && map.containsKey(stones[i] + step)) {
-                        map.get(stones[i] + step).add(step);
+            int pos = stones[i];
+            for (int k : map.get(pos)) {
+                for (int j = k - 1; j <= k + 1; j++) {
+                    if (j > 0 && map.containsKey(pos + j)) {
+                        map.get(pos + j).add(j);
                     }
                 }
             }
