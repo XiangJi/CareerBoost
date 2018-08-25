10bcfda16b4c212cfbd1b4c018056af6c9bf5c99
Xiang Ji <xiang719@gmail.com>
2018-08-25 09:52:53 -0400

    u

--- src/DSA/Tree/ConstructBinaryTreefromPreorderandPostorderTraversal/Solution.java
+++ src/DSA/Tree/ConstructBinaryTreefromPreorderandPostorderTraversal/Solution.java
@@ -16,12 +16,14 @@ public class Solution {
         TreeNode root = new TreeNode(pre[ps]); // 定义开始root
         if(ps + 1 > pe) return root; // 退出条件
                 // this is the start of the left tree
-        int val = pre[ps + 1], idx = pps;
-        for(; idx < ppe; idx++) {
-            if(post[idx] == val) break;
+        int val = pre[ps + 1], index = 0;
+        for(int i = pps; i <= ppe; i++) {
+            if(post[i] == val) {
+               index = i;  
+            }
         }
-        root.left = dfs(pre, ps + 1, ps + idx - pps + 1, post, pps, idx); // 找到了长度
-        root.right = dfs(pre, ps + idx - pps + 2, pe, post, idx + 1, ppe - 1);
+        root.left = dfs(pre, ps + 1, ps + index - pps + 1, post, pps, index); // 找到了长度
+        root.right = dfs(pre, ps + index - pps + 2, pe, post, index + 1, ppe - 1);
         return root;
     }
 }
