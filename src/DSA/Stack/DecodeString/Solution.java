7f252a0a1ef1516d92985f36a5f60b998451047a
Xiang Ji <xiang719@gmail.com>
2018-08-26 00:26:26 -0400

    u

--- src/DSA/Stack/DecodeString/Solution.java
+++ src/DSA/Stack/DecodeString/Solution.java
@@ -22,47 +22,53 @@ stack应用实现题
 
 
 当然需要用stack来辅助运算，我们用两个stack，一个用来保存个数
-，一个用来保存字符串，我们遍历输入字符串，如果遇到数字，我们更新计数变量cnt；如果遇到左中括号，
-我们把当前cnt压入数字栈中，把当前t压入字符串栈中；如果遇到右中括号时，我们取出数字栈中顶元素，
-存入变量k，然后给字符串栈的顶元素循环加上k个t字符串，然后取出顶元素存入字符串t中；如果遇到字母，
-我们直接加入字符串t中即可
+，一个用来保存字符串，我们遍历输入字符串，
+如果遇到数字，我们更新计数变量cnt；
+如果遇到左中括号，
+我们把当前cnt压入数字栈中，把当前t压入字符串栈中；
+
+如果遇到右中括号时，我们取出数字栈中顶元素，
+存入变量k，然后给字符串栈的顶元素循环加上k个t字符串，
+
+然后取出顶元素存入字符串t中；
+如果遇到字母，我们直接加入字符串t中即可
 
 res来存临时数组
 两个stack
  */
-public class Solution {
+class Solution {
     public String decodeString(String s) {
-        String res = "";
-        Stack<Integer> countStack = new Stack<>();
-        Stack<String> resStack = new Stack<>();
-        int idx = 0;
-        while (idx < s.length()) {
-            if (Character.isDigit(s.charAt(idx))) {
+        String temp = "";
+        Stack<Integer> cnt = new Stack<>();
+        Stack<String> stack = new Stack<>();
+        
+        int i = 0;
+        while (i < s.length()) {
+            if (Character.isDigit(s.charAt(i))) {
                 int count = 0;
-                while (Character.isDigit(s.charAt(idx))) {
-                    count = 10 * count + (s.charAt(idx) - '0');
-                    idx++;
+                while (Character.isDigit(s.charAt(i))) {
+                    count = 10 * count + (s.charAt(i) - '0');
+                    i++;
                 }
-                countStack.push(count);
-            }
-            else if (s.charAt(idx) == '[') {
-                resStack.push(res);
-                res = "";
-                idx++;
-            }
-            else if (s.charAt(idx) == ']') {
-                StringBuilder temp = new StringBuilder (resStack.pop());
-                int repeatTimes = countStack.pop();
-                for (int i = 0; i < repeatTimes; i++) {
-                    temp.append(res);
+                cnt.push(count);
+            } else if (s.charAt(i) == '[') {
+                stack.push(temp);
+                temp = "";
+                i++;
+            } else if (s.charAt(i) == ']') {
+                StringBuilder sb = new StringBuilder(stack.pop()); // use previous base
+                int k = cnt.pop();
+                while (k > 0) {
+                    sb.append(temp);
+                    k--;
                 }
-                res = temp.toString();
-                idx++;
-            }
-            else {
-                res += s.charAt(idx++);
+                temp = sb.toString();
+                i++;
+            } else {
+                temp += s.charAt(i);
+                i++;
             }
         }
-        return res;
+        return temp;
     }
 }
